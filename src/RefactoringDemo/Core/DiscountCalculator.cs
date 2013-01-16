fc72b48a29f6b0fde620676cc44984a6524b01a6
Peter Seale <peter@pseale.com>
2013-01-16 03:38:12 -0600

    move business logic out of UI code

---
+++ src/RefactoringDemo/Core/DiscountCalculator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RefactoringDemo.Core
+{
+    public static class DiscountCalculator
+    {
+        public static decimal Calculate(IEnumerable<int> books)
+        {
+
+            //if the book set is empty, return 0
+            if (books == null || books.Count() == 0)
+                return 0m;
+
+            decimal runningTotal = 0m;
+            var remainingBooks = new List<int>(books);
+            //while we have books, add to the running total
+            while (remainingBooks.Count > 0)
+            {
+                //create a grouped list of books by book title
+                var groups = remainingBooks.GroupBy(bookId => bookId);
+
+                //find the # of book titles we have
+                var uniqueBooksCount = groups.Count();
+
+                //calculate the discount for this group
+                decimal percentDiscounted;
+                switch (uniqueBooksCount)
+                {
+                    case 2:
+                        percentDiscounted = 0.05m;
+                        break;
+                    case 3:
+                        percentDiscounted = 0.10m;
+                        break;
+                    case 4:
+                        percentDiscounted = 0.20m;
+                        break;
+                    case 5:
+                        percentDiscounted = 0.25m;
+                        break;
+                    default:
+                        percentDiscounted = 0m;
+                        break;
+                }
+
+                //add to the running total
+                runningTotal += uniqueBooksCount * 8m * (1m - percentDiscounted);
+
+                //remove one book for each title
+                foreach (var bookIdGroup in groups)
+                {
+                    int bookId = bookIdGroup.Key;
+                    remainingBooks.Remove(bookId);
+                }
+            }
+
+            //return the sum
+            return runningTotal;
+        }
+    }
+}
--- src/RefactoringDemo/UI/ShellViewModel.cs
+++ src/RefactoringDemo/UI/ShellViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using Caliburn.Micro;
+using RefactoringDemo.Core;
 
 namespace RefactoringDemo.UI
 {
@@ -27,56 +28,7 @@ namespace RefactoringDemo.UI
         private decimal CalculateGrandTotal()
         {
             var books = GetBooksList();
-
-            //if the book set is empty, return 0
-            if (books == null || books.Count() == 0)
-                return 0m;
-
-            decimal runningTotal = 0m;
-            var remainingBooks = new List<int>(books);
-            //while we have books, add to the running total
-            while (remainingBooks.Count > 0)
-            {
-                //create a grouped list of books by book title
-                var groups = remainingBooks.GroupBy(bookId => bookId);
-
-                //find the # of book titles we have
-                var uniqueBooksCount = groups.Count();
-
-                //calculate the discount for this group
-                decimal percentDiscounted;
-                switch (uniqueBooksCount)
-                {
-                    case 2:
-                        percentDiscounted = 0.05m;
-                        break;
-                    case 3:
-                        percentDiscounted = 0.10m;
-                        break;
-                    case 4:
-                        percentDiscounted = 0.20m;
-                        break;
-                    case 5:
-                        percentDiscounted = 0.25m;
-                        break;
-                    default:
-                        percentDiscounted = 0m;
-                        break;
-                }
-
-                //add to the running total
-                runningTotal += uniqueBooksCount * 8m * (1m - percentDiscounted);
-
-                //remove one book for each title
-                foreach (var bookIdGroup in groups)
-                {
-                    int bookId = bookIdGroup.Key;
-                    remainingBooks.Remove(bookId);
-                }
-            }
-
-            //return the sum
-            return runningTotal;
+            return DiscountCalculator.Calculate(books);
         }
 
         private List<int> GetBooksList()
