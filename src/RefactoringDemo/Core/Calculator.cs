f3f32d0bf726271f03789fe58e6edb87427253f8
Peter Seale <peter@pseale.com>
2013-01-16 15:10:24 -0600

    extract method to calculate book set discount

--- src/RefactoringDemo/Core/Calculator.cs
+++ src/RefactoringDemo/Core/Calculator.cs
@@ -22,26 +22,7 @@ namespace RefactoringDemo.Core
                 //find the # of book titles we have
                 var uniqueBooksCount = groups.Count();
 
-                //calculate the discount for this group
-                decimal percentDiscounted;
-                switch (uniqueBooksCount)
-                {
-                    case 2:
-                        percentDiscounted = 0.05m;
-                        break;
-                    case 3:
-                        percentDiscounted = 0.10m;
-                        break;
-                    case 4:
-                        percentDiscounted = 0.20m;
-                        break;
-                    case 5:
-                        percentDiscounted = 0.25m;
-                        break;
-                    default:
-                        percentDiscounted = 0m;
-                        break;
-                }
+                var percentDiscounted = GetDiscountFor(uniqueBooksCount);
 
                 //add to the running total
                 runningTotal += uniqueBooksCount * 8m * (1m - percentDiscounted);
@@ -56,6 +37,30 @@ namespace RefactoringDemo.Core
 
             //return the sum
             return runningTotal;
+        }
+
+        private static decimal GetDiscountFor(int bookSetSize)
+        {
+            decimal percentDiscounted;
+            switch (bookSetSize)
+            {
+                case 2:
+                    percentDiscounted = 0.05m;
+                    break;
+                case 3:
+                    percentDiscounted = 0.10m;
+                    break;
+                case 4:
+                    percentDiscounted = 0.20m;
+                    break;
+                case 5:
+                    percentDiscounted = 0.25m;
+                    break;
+                default:
+                    percentDiscounted = 0m;
+                    break;
+            }
+            return percentDiscounted;
         }
     }
 }
